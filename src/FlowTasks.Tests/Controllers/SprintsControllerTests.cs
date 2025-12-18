7217fa5f016a328d22c7cf429b41acfa7b265722
faiez_benamar <38057044+benamarfaiez@users.noreply.github.com>
2025-12-18 21:56:21 +0100

    Refonte des tests Projects/ProjectMembers + squelette Sprints
    Refonte complète des tests unitaires pour ProjectsController et ProjectMembersController : utilisation d'AutoFixture et Moq, couverture de tous les scénarios principaux (authentification, autorisation, erreurs, etc.), suppression des anciens tests factices. Ajout du fichier SprintsControllerTests.cs (classe vide pour l’instant).

---
+++ src/FlowTasks.Tests/Controllers/SprintsControllerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowTasks.Tests.Controllers
+{
+    internal class SprintsControllerTests
+    {
+    }
+}
